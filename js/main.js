1249c7a98863b997c80132c9c183ed8b5621b6c0
umardeveloper <88134771+umardeveloper@users.noreply.github.com>
2021-10-08 08:36:19 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 const elHamburgerBtn = document.querySelector('.header-humburger-button');
 const elHeader = document.querySelector('.header');
-
-
-
 elHamburgerBtn.addEventListener('click', (evt) => {
     elHeader.classList.toggle('header-open');
 });
